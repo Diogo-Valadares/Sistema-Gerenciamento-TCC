fbfa9e9ddfa3bab7d2dfb6e9452d0e5f4e94d00a
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2024-08-31 19:09:19 -0300

    runnner

---
+++ src/main/java/com/GerenciadorTCC/Runner.java
@@ -0,0 +1,34 @@
+package com.GerenciadorTCC;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner{
+/* 
+    @Autowired
+    PersonRepository personRepository;
+    @Autowired
+    AcademicWorkRepository academicWorkRepository;    
+    @Autowired
+    AdvisorRepository advisorRepository;
+    @Autowired
+    AvaliationRepository avaliationRepository;
+    @Autowired
+    DocumentRepository documentRepository;    
+    @Autowired
+    StudentRepository studentRepository;
+    @Autowired
+    TaskRepository taskRepository;
+    @Autowired
+    TaskDeliverRepository taskDeliverRepository;
+    @Autowired
+    WorkTypeRepository workTypeRepository;
+*/
+
+    @Override
+    public void run(String... args) throws Exception {   
+        //advisorRepository.save(advisor);
+    }
+    
+}
