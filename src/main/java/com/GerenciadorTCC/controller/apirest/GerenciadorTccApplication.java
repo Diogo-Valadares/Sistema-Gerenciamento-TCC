330ae112ce3a7b5077b570e9c113479c765a3ae7
Diogo Valadares Reis dos Santos <58271577+Diogo-Valadares@users.noreply.github.com>
2024-10-07 14:42:01 -0600

    correção de nome de pasta

---
+++ src/main/java/com/GerenciadorTCC/controller/apirest/GerenciadorTccApplication.java
@@ -0,0 +1,25 @@
+package com.GerenciadorTCC.controller.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan({
+	"com.GerenciadorTCC.controller.apirest",
+	"com.GerenciadorTCC.controller.view",
+	"com.GerenciadorTCC.service",
+	"com.GerenciadorTCC"}
+)
+@EntityScan("com.GerenciadorTCC.entities")
+@EnableJpaRepositories("com.GerenciadorTCC.repository")
+public class GerenciadorTccApplication extends SpringBootServletInitializer{
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorTccApplication.class, args);
+	}
+}
